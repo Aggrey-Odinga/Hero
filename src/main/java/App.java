eb981679fcf89d809f961068371a018c8ecafdf9
Aggrey-Odinga <aggreyodinga@gmail.com>
2022-05-04 16:15:02 +0300

    Modified heroes page and optimized styles

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import exceptions.HeroExistsException;
+import exceptions.SquadExistsException;
 import models.Hero;
 import models.Squad;
 import spark.ModelAndView;
@@ -13,7 +15,15 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (request, response) -> {
@@ -45,13 +55,33 @@ public class App {
             hero.addPower(power);
             hero.addWeakness(weakness);
 
-            HeroService heroService = new HeroService();
-            herosDB = heroService.addHero(hero, herosDB);
+
+            String message = "hero created successfully";
+
+            try {
+                HeroService heroService = new HeroService();
+                herosDB = heroService.addHero(hero, herosDB);
+            } catch (HeroExistsException e) {
+                message = e.getMessage();
+            }
 
             request.session().attribute("heros", herosDB);
             model.put("heros", herosDB);
+            model.put("message", message);
             return new ModelAndView(model, "heroes.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<Hero> herosDB = request.session().attribute("heros");
+            model.put("heros", herosDB);
+            return new ModelAndView(model, "heroes.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
 
 
         get("/squad", (request, response) -> {
@@ -68,10 +98,26 @@ public class App {
             List<Squad> squadsDB = request.session().attribute("squads");
 
             Squad squad = new Squad(name, cause, Integer.parseInt(size));
-            SquadService squadService = new SquadService();
-            squadsDB = squadService.addSquad(squadsDB, squad);
+
+
+            String message = "squad created successfully";
+
+            try {
+                SquadService squadService = new SquadService();
+                squadsDB = squadService.addSquad(squadsDB, squad);
+            } catch (SquadExistsException e) {
+                message = e.getMessage();
+            }
 
             request.session().attribute("squads", squadsDB);
+            model.put("squads", squadsDB);
+            return new ModelAndView(model, "squads.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<Squad> squadsDB = request.session().attribute("squads");
             model.put("squads", squadsDB);
             return new ModelAndView(model, "squads.hbs");
         }, new HandlebarsTemplateEngine());
